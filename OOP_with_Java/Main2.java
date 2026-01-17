1711a92ed68c1d6a8980379676fe09426426e08f
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-01-17 18:36:00 +0600

    Main2.java

---
+++ OOP_with_Java/Main2.java
@@ -0,0 +1,10 @@
+public class Main2 {
+    int x = 500;
+    public static void main(String[] args) {
+     
+        Main2 myObj = new Main2();
+        System.out.println(myObj.x);
+
+    }
+    
+}
