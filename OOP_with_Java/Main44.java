fac0fbc601e9f1225c1a60b41faf5481592f0ef9
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-01-20 19:42:55 +0600

    Add Main44 and Main8

---
+++ OOP_with_Java/Main44.java
@@ -0,0 +1,7 @@
+public class Main44 {
+    public static void main(String[] args){
+        Main4 myCar = new Main4();
+        myCar.fullThrottle();
+        myCar.speed(200);
+    }
+}
---
+++ OOP_with_Java/Main8.java
@@ -0,0 +1,17 @@
+class Person{
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String newName){
+        
+    }
+}
+public class Main8 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
