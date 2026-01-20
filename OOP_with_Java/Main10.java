db5ef5202ad0c5607d044c8d1da8f410b2039808
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-01-20 19:49:31 +0600

    Main10.java

---
+++ OOP_with_Java/Main10.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main10 {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        String userName = myObj.nextLine();
+        System.out.println(userName);
+    }
+    
+}
