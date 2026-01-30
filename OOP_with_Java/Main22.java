46e1713a1f1fee38491a3543802c7f97fdf1c282
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-01-30 14:29:08 +0600

    Enum inside a Class

---
+++ OOP_with_Java/Main22.java
@@ -0,0 +1,14 @@
+
+public class Main22 {
+    enum Level{
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+
+    public static void main(String[] args) {
+        Level myVar = Level.MEDIUM;
+        System.out.println(myVar);
+    }
+    
+}
