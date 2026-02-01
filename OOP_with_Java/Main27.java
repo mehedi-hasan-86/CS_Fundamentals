3798e86ae6f43295b9ac625def15e6e9abc56f4f
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-02-01 20:46:55 +0600

    Display Current Date and Time

---
+++ OOP_with_Java/Main27.java
@@ -0,0 +1,18 @@
+// import java.time.LocalDate;
+// import java.time.LocalTime;
+
+import java.time.LocalDateTime;
+
+public class Main27 {
+    public static void main(String[] args) {
+        // LocalDate myObj = LocalDate.now();
+        // System.out.println(myObj);
+
+        // LocalTime timeObj = LocalTime.now();
+        // System.out.println(timeObj);
+
+        LocalDateTime timeDateObj = LocalDateTime.now();
+        System.out.println(timeDateObj);
+    }
+    
+}
