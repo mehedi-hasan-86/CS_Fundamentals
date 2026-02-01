ea4751e669a24fea9bd21276d4882575bc29a970
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-02-01 20:31:13 +0600

    Java User Input (Scanner)

---
+++ OOP_with_Java/Main25.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main25 {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner((System.in));
+        String userName = myObj.nextLine();
+        System.out.println(userName);
+    }
+    
+}
