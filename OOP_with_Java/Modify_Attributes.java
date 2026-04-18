972ec9704ad398f0bba2e773b64ab849d98cf3f9
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-04-18 12:30:18 +0600

    Modify_Attributes.java

---
+++ OOP_with_Java/Modify_Attributes.java
@@ -0,0 +1,10 @@
+public class Modify_Attributes {
+    int x ;
+
+    public static void main(String[] args) {
+        Modify_Attributes myObj = new Modify_Attributes();
+        myObj.x  = 40;
+        System.out.println(myObj.x);
+    }
+    
+}
