a6bfcd38b1394f612af5a3d3a3a00cb60dd544fc
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-04-18 12:01:43 +0600

    Create_class_and_object.java

---
+++ OOP_with_Java/Create_class_and_object.java
@@ -0,0 +1,9 @@
+public class Create_class_and_object {
+    
+    int x = 5;
+
+    public static void main(String[] args){
+        Create_class_and_object myObj = new Create_class_and_object();
+        System.out.println(myObj.x);
+    }
+}
