6e7e00f528da49d701f2ff97f8d35a62853d917b
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-04-18 12:46:23 +0600

    Java_Class_Code_Challenge.java

---
+++ OOP_with_Java/Java_Class_Code_Challenge.java
@@ -0,0 +1,11 @@
+public class Java_Class_Code_Challenge {
+    String city = "London";
+    int population = 900000;
+
+    public static void main(String[] args) {
+        Java_Class_Code_Challenge myObj = new Java_Class_Code_Challenge();
+        System.out.println(myObj.city);
+        System.out.println(myObj.population);
+    }
+    
+}
