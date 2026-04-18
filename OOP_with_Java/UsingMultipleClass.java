69e244c1e63ba866bbd415d5d4a5e9aad102c996
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-04-18 12:25:59 +0600

    UsingMultipleClass.java

---
+++ OOP_with_Java/UsingMultipleClass.java
@@ -0,0 +1,11 @@
+class Main{
+    int x = 5;
+}
+
+public class UsingMultipleClass {
+    public static void main(String[] args) {
+        Main obj = new Main();
+        System.out.println(obj.x);
+    }
+    
+}
