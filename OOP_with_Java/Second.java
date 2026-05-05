cb4e8ba5dcf7a4e63c21390cd9dcc087961985b9
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-05-05 21:32:27 +0600

    Add Second.java

---
+++ OOP_with_Java/Second.java
@@ -0,0 +1,10 @@
+class Usingmultipelclass {
+    int x = 5;
+}
+
+public class Second {
+    public static void main(String[] args) {
+        Usingmultipelclass obj = new Usingmultipelclass();
+        System.out.println(obj.x);
+    }
+}
